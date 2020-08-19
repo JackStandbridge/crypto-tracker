5fe04b899a6e6d0f625915b1d8ffd93025d13abd
Jack Standbridge <jack.s.standbridge@gmail.com>
2020-08-19 21:59:28 +0100

    add material-ui Select component to replace currency picker
    Native selects are very time consuming to style, so I have added a component from material-ui to do the job. Unfortunately it seems to trigger a React warning in this commit, but this doesn't seem to hinder functionality

--- src/components/CurrencyPicker/CurrencyPicker.js
+++ src/components/CurrencyPicker/CurrencyPicker.js
@@ -1,27 +1,24 @@
 import React from 'react';
+import { Select } from '@material-ui/core';
 
 import stylesheet from './CurrencyPicker.module.scss';
 
 const CurrencyPicker = ({ options, value, handleChange }) => {
-	return (
-		<>
-			<select
-				className={ stylesheet.select }
-				onChange={ handleChange }
-				value={ value }
-			>
-
-				{ options.map(currency => (
-					<option
-						value={ currency }
-						key={ currency }
-					>
-						{ currency }
-					</option>
-				)) }
-
-			</select>
-		</>
+	return value && (
+		<Select
+			className={ stylesheet.select }
+			onChange={ handleChange }
+			value={ value }
+		>
+			{ options.map(currency => (
+				<option
+					value={ currency }
+					key={ currency }
+				>
+					{ currency }
+				</option>
+			)) }
+		</Select>
 	);
 };
 
