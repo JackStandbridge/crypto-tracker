e40a48912437b258dd4dfda8bfa1db7fe55e44f4
Jack Standbridge <jack.s.standbridge@gmail.com>
2020-08-19 22:28:36 +0100

    Updates

---
+++ precache-manifest.dd03eece6eb03035aa359ad58f16dec0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8549664e3aa08e99668e420f0e8393",
+    "url": "/crypto-tracker/index.html"
+  },
+  {
+    "revision": "96755322d755547d8d7c",
+    "url": "/crypto-tracker/static/css/main.aa7702b5.chunk.css"
+  },
+  {
+    "revision": "da9698c2c0721773f69e",
+    "url": "/crypto-tracker/static/js/2.7a9f5851.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/crypto-tracker/static/js/2.7a9f5851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96755322d755547d8d7c",
+    "url": "/crypto-tracker/static/js/main.a8ed3ff2.chunk.js"
+  },
+  {
+    "revision": "3046e15a071d6251821d",
+    "url": "/crypto-tracker/static/js/runtime-main.50d108b2.js"
+  },
+  {
+    "revision": "3be8826ee26bed57829ce560fd493ad5",
+    "url": "/crypto-tracker/static/media/back.3be8826e.svg"
+  },
+  {
+    "revision": "ac4471291c620d8dd47697a1d8da4975",
+    "url": "/crypto-tracker/static/media/loading.ac447129.gif"
+  }
+]);
